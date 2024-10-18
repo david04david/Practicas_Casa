4c418a8dbd906bf7a16f6dfad6cfd84af4b44b31
David García <dgarciar27@educarex.es>
2024-10-18 09:47:44 +0000

    a

--- Practica API/app.js
+++ Practica API/app.js
@@ -70,3 +70,127 @@ function mostrarTiempo(data){
 
 obtenerTiempo(lat,lon);
 reloj();
+
+//Funciones Formulario
+let campoName = document.querySelector('#name');
+let campoDNI = document.querySelector('#dni');
+let campoPhone = document.querySelector('#phone');
+let campoEmail = document.querySelector('#email');
+let campoEdad = document.querySelector('#edad');
+let btnSubmit = document.querySelector('#submit');
+const formPrueba = document.querySelector('#formPrueba');
+
+const zonaSaludo = document.querySelector('#saludo');
+
+let contacts = [{
+    name: "Maxwell Wright",
+    phone: "(0191) 719 6495",
+    email: "Curabitur.egestas.nunc@nonummyac.co.uk",
+    edad: 20,
+    cat:"Adult"
+    }, {
+    name: "Raja Villarreal",
+    phone: "0866 398 2895",
+    email: "posuere.vulputate@sed.com",
+    edad: 15,
+    cat:"Teenager"
+    }, {
+    name: "Helen Richards",
+    phone: "0800 1111",
+    email: "libero@convallis.edu",
+    edad:19,
+    cat:"Teenager"
+    }];
+
+function addPerson(){
+    let valorName = campoName.value;
+    let valorDNI = campoDNI.value;
+    let valorPhone = campoPhone.value;
+    let valorEmail = campoEmail.value;
+    let valorEdad = campoEdad.value;
+    let valorCategoria;
+    
+    const pattern = /^[0-9]{8}[A-Za-z]{1}$/;
+
+    if(valorName==""){
+        alert("El nombre esta vacio");
+        return false;
+    }
+
+    if(!pattern.test(valorDNI)){
+        alert("Por favor, introduzca un DNI válido (8 dígitos y 1 letra).");
+        return false;
+    }
+
+    switch (true) {
+        case valorEdad>0&&valorEdad<=12:
+                valorCategoria="Child";
+            break;
+
+        case valorEdad>=13&&valorEdad<=19:
+                valorCategoria="Teenager";
+            break;
+
+        case valorEdad>=20&&valorEdad<=64:
+                valorCategoria="Adult";
+            break;
+        
+        case valorEdad>=65:
+                valorCategoria="Senior";
+            break;
+    
+        default:""
+            break;
+    }
+
+        contacts.push({
+            name:valorName,
+            DNI:valorDNI,
+            phone:valorPhone,
+            email:valorEmail,
+            edad:valorEdad,
+            categoria:valorCategoria,
+        });
+        
+        let last = contacts.length - 1; 
+        console.log(contacts[last]);
+        return true;
+       /* formPrueba.reset();*/
+    //}
+
+    
+}
+
+function showPersons(){
+    for (persona of contacts) {
+        console.log(persona);
+        if(persona.edad<18){
+            alert(persona.name+" es menor de edad");
+        }
+    }
+}
+
+function logPersonRecursive(index){
+    /*let index = 0;*/
+    if(index<contacts.length){
+       /*if(index<contacts.length){
+        console.log(contacts[index]);
+    } 
+        index ++;*/
+        console.log(contacts[index]);
+        logPersonRecursive(index+1);
+    }
+}
+
+function enviar(){
+
+    if (addPerson()){
+        //generar cadena json
+        //pasar datos json a fichero
+        let jsonString = JSON.stringify(contacts);
+        console.log(jsonString);
+        //writeFile
+        formPrueba.submit();
+    }else    
+        window.alert("algo raro");
+}
