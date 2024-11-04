c40d8f58a119b3a8551af8d32f4a3f9142c341ae
David García <dgarciar27@educarex.es>
2024-11-04 11:54:04 +0000

    a

--- Practica API/app.js
+++ Practica API/app.js
@@ -221,6 +221,7 @@ function showPersons(){
 
 function muestraInfo() {
     for(persona of contacts){
+        campoInfo.innerHTML = "";
         campoInfo.innerHTML +=`${persona.name} ${persona.edad}<br>`;
         if(persona.edad<18){
             alert(persona.name+" es menor de edad");
